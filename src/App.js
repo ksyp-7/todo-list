37668f85485e3127484bd07af8dd512adf6408ca
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-24 11:58:11 +0530

    Implemented State and randring data using map method

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos ,setTodas] = useState(['1','2','3','4']);
   return (
     <div className="App">
       <h1>Let's get started</h1>
+      <input />
+      <button>Todo</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+
+      </ul>
     </div>
   );
 }
