564fb735a13a74aaec6dd9ad9535bbcad01afec6
shukl kashyap <shuklakashyap777777@gmail.com>
2021-01-28 12:07:50 +0530

    Delete Button

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,7 @@
 import { List,ListItem, ListItemAvatar, ListItemText } from '@material-ui/core'
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import db from './firebase';
 
 function Todo(props) {
 	return (
@@ -7,8 +9,9 @@ function Todo(props) {
 	<ListItem>
 	<ListItemAvatar>
 	</ListItemAvatar>
-	<ListItemText primary={props.text} secondary="Dummy Dead-Line" />
+	<ListItemText primary={props.todo.todo} secondary="Dummy Dead-Line" />
 	</ListItem>
+	<Button onClick={event => db.collection('todos').doc(props.todo.id).delete()}>🇽 Delete</Button>
 	</List>
 	)
 
